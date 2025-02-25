1e8499e5d65901190d0fa651e2077d4a68434e59
Mark Kevin Cagandahan <mark.cagandahan@kooapps.com>
2025-02-26 02:30:46 +0800

    Authentication: Add early validation check for credentials when logging in

--- LoginApp/Service/Authentication/SampleAuthenticationService.swift
+++ LoginApp/Service/Authentication/SampleAuthenticationService.swift
@@ -12,6 +12,11 @@ class SampleAuthenticationService: AuthenticationService {
     let password = "12345"
     
     func login(with credentials: Credentials, completion: @escaping (Result<Bool, AuthenticationError>) -> Void) {
+        if !isValid(credentials) {
+            completion(.failure(.invalidCredentials))
+            return
+        }
+        
         DispatchQueue.global().asyncAfter(deadline: .now() + 1.5) {
             if credentials.username == self.username && credentials.password == self.password {
                 completion(.success(true))
@@ -28,4 +33,7 @@ class SampleAuthenticationService: AuthenticationService {
         }
     }
     
+    func isValid(_ credentials: Credentials) -> Bool {
+        !credentials.username.isEmpty && !credentials.password.isEmpty
+    }
 }
