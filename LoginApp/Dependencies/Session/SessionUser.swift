e1f8396f2b6271ef1fbf483bc5d19d17146f5dba
Mark Kevin Cagandahan <mark.cagandahan@kooapps.com>
2025-02-26 09:40:37 +0800

    Authentication: Add custom init for SessionUser with dictionary as parameter (for handling API response)

--- LoginApp/Dependencies/Session/SessionUser.swift
+++ LoginApp/Dependencies/Session/SessionUser.swift
@@ -10,4 +10,14 @@ import Foundation
 struct SessionUser {
     let id: String
     let token: String
+    
+    init?(from dictionary: [String: Any]) {
+        guard let id = dictionary["id"] as? String,
+              let token = dictionary["token"] as? String else {
+            return nil
+        }
+        
+        self.id = id
+        self.token = token
+    }
 }
